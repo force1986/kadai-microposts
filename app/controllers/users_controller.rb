dd668589d6fe46c6748437cc1fc718da1ec2a5b6
hiroyuki.okino <force.21.1986@gmail.com>
2019-04-05 05:59:22 +0000

    follow, unfollow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
+  
   def index
     @users = User.all.page(params[:page])
   end
@@ -17,7 +18,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     
-    if @eser.save
+    if @user.save
       flash[:success] = 'ユーザを登録しました'
       redirect_to @user
     else
@@ -26,6 +27,18 @@ class UsersController < ApplicationController
     end
   end
   
+  def followings
+    @user = User.find(params[:id])
+    @followings = @user.followings.page(params[:page])
+    counts(@user)
+  end
+
+  def followers
+    @user = User.find(params[:id])
+    @followers = @user.followers.page(params[:page])
+    counts(@user)
+  end  
+    
   private
   
   def user_params
